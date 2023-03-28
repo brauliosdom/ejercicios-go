7b594f05dea1b43349ba6c9241fbf50455e0ce96
Laura Marcela Arango Correa <laura.arango@mercadolibre.com.co>
2023-03-28 11:19:20 -0500

    ejercicio 2 cuarta clase

---
+++ bootcamp/clase4/ejercicio2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	ID int
+	Name string
+	DateOfBirth string
+}
+
+type Employee struct {
+	ID int
+	Position string
+	Person
+}
+
+func main() {
+	p1 := Person{
+		ID: 1,
+		Name: "Laura",
+		DateOfBirth: "1998-01-01",
+	}
+
+	e1 := Employee{
+		ID: 1,
+		Position: "Developer",
+		Person: p1,
+	}
+
+	e1.PrintEmployed()
+}
+
+func (e Employee) PrintEmployed(){
+	fmt.Println(e)
+}
